53ce1d534cd760555d8583d1d2e46f8f49e5baa0
nirmalayaradi <154364164+nirmalayaradi@users.noreply.github.com>
2024-02-10 02:47:15 +0530

    Add files via upload

---
+++ Todos/login.js
@@ -0,0 +1,38 @@
+document.querySelector("form").addEventListener("submit",goToHome);
+let users = JSON.parse(localStorage.getItem("users"))
+console.log(users)
+
+function goToHome(event){
+    event.preventDefault();
+    let emailId=document.querySelector("#ipEmailPhone").value;
+
+    let isValidUser
+    if (users !== null){
+        isValidUser = users.find((user)=>{
+            if (user.email === emailId){
+                return true
+            }
+        })
+    }
+    
+    
+    if(users !== null && isValidUser !== undefined){
+        users = users.map((user) => {
+            if (user.email === emailId){
+                return {...user,isLogin: true}
+            }else{
+                return user
+            }
+        })
+
+        localStorage.setItem("users", JSON.stringify(users))
+        window.location.href="todo.html"
+    } else {
+        alert("Wrong Credentials  (If you don't have account click signup)")
+    } 
+}
+
+document.querySelector("#gotoSignUp").addEventListener("click",gotoSignUp);
+function gotoSignUp(){
+    window.location.href="signup.html"
+}
